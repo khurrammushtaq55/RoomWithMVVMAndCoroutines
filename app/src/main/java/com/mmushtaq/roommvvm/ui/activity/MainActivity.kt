af7924fc9f71504f1b41a6dc59df14ac52ff7027
mmushtaq04 <mmushtaq04@i2cinc.com>
2022-09-09 15:10:03 +0500

    new files

---
+++ app/src/main/java/com/mmushtaq/roommvvm/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mmushtaq.roommvvm.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mmushtaq.roommvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/mmushtaq/roommvvm/view_model/fragment/LoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.mmushtaq.roommvvm.view_model.fragment
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+}
